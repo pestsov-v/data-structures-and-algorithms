b94603ec53ed5ccea3f1791000a4897af474518d
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-08 17:49:17 +0300

    string reverse methods

--- string-algorithms/string_reversal.js
+++ string-algorithms/string_reversal.js
@@ -1,10 +1,38 @@
-
 // Разворачивает строку с помощью массива и метода reverse
-function reverse(str) {
+function reverse1(str) {
     const arr = str.split('')
     arr.reverse()
     return arr.join('')
 }
 
-const str1 = reverse('Vlad')
+const str1 = reverse1('Vlad')
 console.log("str1", str1)
+
+function reverse2(str) {
+    return str.split('').reverse().join('')
+}
+
+const str2 = reverse2('Vlad')
+console.log("str2", str2)
+
+function reverse3(str) {
+    let reversed = ''
+
+    for (let character of str) {
+        reversed = character + reversed
+    }
+
+    return reversed
+}
+
+const str3 = reverse3('Vlad')
+console.log("str3", str3)
+
+function reverse4(str) {
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed
+    }, '')
+}
+
+const str4 = reverse4('Vlad')
+console.log("str4", str4)
